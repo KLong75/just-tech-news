b79222eb8f13bf30e685fc3dd10974693d9e1649
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-05-03 15:31:42 -0500

    add comment model and routes

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Comment, Post, User, } = require('../../models');
+
+router.get('/', (req, res) => {
+  Comment.findAll({
+        attributes: [
+          'id', 
+          'comment_text', 
+          'user_id', 
+          'post_id', 
+          'created_at'
+        ], 
+        order: [['created_at', 'DESC']],
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          },
+          {
+            model: Post,
+            attributes: ['username', 'post_title']
+          },
+        ]
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        }); 
+
+router.post('/', (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+      user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+       });
+    });
+});
+
+router.delete('/:id', (req,res) => {
+  Comment.destroy({
+    where: {
+    id: req.params.id
+    }
+  })
+  .then(dbCOmmentData => {
+    if (!dbCommentData) {
+    res.status(404).json({ message: 'No post found witht this id' });
+      return;
+    }
+      res.json(dbCommentData);
+    })
+      .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+      }); 
+});
+  
+
+module.exports = router;
