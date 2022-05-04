4b330998c7cb9d507005fc8222922a9cb14edc0c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-05-04 16:46:59 -0500

    add logout route and functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,27 +3,28 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    Post.findAll({
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+  console.log(req.session);
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+    include: [
+      {
+      model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
           model: User,
           attributes: ['username']
         }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
       ]
     })
       .then(dbPostData => {
@@ -36,5 +37,13 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});  
 
 module.exports = router;
