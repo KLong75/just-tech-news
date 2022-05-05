8af804fa7c8da81106db246cef9f58742a5e3179
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-05-05 10:19:24 -0500

    feature/single-post

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,45 +1,29 @@
 const router = require('express').Router();
-const { Comment, Post, User, } = require('../../models');
+const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-  Comment.findAll({
-        attributes: [
-          'id', 
-          'comment_text', 
-          'user_id', 
-          'post_id', 
-          'created_at'
-        ], 
-        order: [['created_at', 'DESC']],
-        include: [
-          {
-            model: User,
-            attributes: ['username']
-          },
-          {
-            model: Post,
-            attributes: ['username', 'post_title']
-          },
-        ]
-      })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        }); 
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    }); 
+});
 
 router.post('/', (req, res) => {
-  Comment.create({
-    comment_text: req.body.comment_text,
-      user_id: req.body.user_id,
-        post_id: req.body.post_id
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      user_id: req.session.user_id,
+      post_id: req.body.post_id
     })
       .then(dbCommentData => res.json(dbCommentData))
       .catch(err => {
         console.log(err);
         res.status(400).json(err);
-       });
-    });
+      });
+  }
 });
 
 router.delete('/:id', (req,res) => {
@@ -48,7 +32,7 @@ router.delete('/:id', (req,res) => {
     id: req.params.id
     }
   })
-  .then(dbCOmmentData => {
+  .then(dbCommentData => {
     if (!dbCommentData) {
     res.status(404).json({ message: 'No post found witht this id' });
       return;
